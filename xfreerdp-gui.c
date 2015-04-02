fb8c8d2b407878f1fd41988a4532715b82cc50b7
Ryan Fore <ryan.b.fore@gmail.com>
2015-04-01 22:37:39 -0500

    Added behavior to pull values from GtkEntries

--- xfreerdp-gui.c
+++ xfreerdp-gui.c
@@ -2,19 +2,34 @@
 
     /*
      * Compile me with:
-     *   gcc -o tut tut.c $(pkg-config --cflags --libs gtk+-2.0 gmodule-2.0)
+     * 	cc -o tut tut.c $(pkg-config --cflags --libs gtk+-2.0 gmodule-2.0)
      */
-    
+    #include <string.h>
     #include <gtk/gtk.h>
 
     GtkBuilder *builder;
 
     int clicker (GtkWidget *widget, gpointer   data)
     { 
-  	gchar *entry_value;
+	char execute[160];
+  	const gchar* server_entry;
+	const gchar* username_entry;
+	const gchar* password_entry;
 
-	entry_value = gtk_entry_get_text(GTK_ENTRY((GtkWidget *) data));
-	g_print("%s\n", entry_value);
+	server_entry = gtk_entry_get_text(GTK_ENTRY((GtkWidget *) gtk_grid_get_child_at((GtkGrid *) data, (gint) 1,(gint) 0 )));
+	username_entry = gtk_entry_get_text(GTK_ENTRY((GtkWidget *) gtk_grid_get_child_at((GtkGrid *) data, (gint) 1,(gint) 1 )));
+	password_entry = gtk_entry_get_text(GTK_ENTRY((GtkWidget *) gtk_grid_get_child_at((GtkGrid *) data, (gint) 1,(gint) 2 )));
+	
+	strcpy(execute, "xfreerdp ");
+	strcat(execute, "/v ");
+	strcat(execute, server_entry);
+	strcat(execute, " ");
+	strcat(execute, "/u ");
+	strcat(execute, username_entry);
+	strcat(execute, " /p ");
+	strcat(execute, password_entry);
+	
+	g_print("%s\n", execute);
     }
  
     int main(int argc, char **argv )
